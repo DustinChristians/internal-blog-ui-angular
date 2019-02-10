cc006b87e1b3c15510407b0e3279c16310442b1d
Dustin <dustinchristians@gmail.com>
2019-02-10 17:26:20 -0500

    Added the ListGroupItem Interface

--- blog/src/app/shared/list-group-item/list-group-item.component.ts
+++ blog/src/app/shared/list-group-item/list-group-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IListGroupItem } from './list-group-item';
 
 @Component({
   selector: 'app-list-group-item',
   templateUrl: './list-group-item.component.html',
   styleUrls: ['./list-group-item.component.scss']
 })
-export class ListGroupItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ListGroupItemComponent {
+  @Input() listGroupItem: IListGroupItem;
 }
---
+++ blog/src/app/shared/list-group-item/list-group-item.ts
@@ -0,0 +1,5 @@
+export interface IListGroupItem {
+  description: string;
+  heading: string;
+  path: string;
+}
