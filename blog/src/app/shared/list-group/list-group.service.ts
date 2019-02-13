e009488d4b8ff79ca9325a83e59d19ce063b8290
Dustin <dustinchristians@gmail.com>
2019-02-13 18:37:11 -0500

    Added exception handling and an error service.

--- blog/src/app/shared/list-group/list-group.service.ts
+++ blog/src/app/shared/list-group/list-group.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { IListGroupItem } from '../list-group-item/list-group-item';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorService } from 'src/app/core/error/error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListGroupService {
-  constructor(private http: HttpClient) {}
+  constructor(private errorService: ErrorService, private http: HttpClient) {}
 
   getListGroups(url: string): Observable<IListGroupItem[]> {
-    return this.http.get<IListGroupItem[]>(url);
+    return this.http
+      .get<IListGroupItem[]>(url)
+      .pipe(catchError(this.errorService.handleError));
   }
 }
