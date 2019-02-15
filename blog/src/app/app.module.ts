f18d80c41d59cb6c8572320232f69f25b4ab1d70
Dustin <dustinchristians@gmail.com>
2019-02-14 19:03:21 -0500

    Split the application up into modules (home, not-found and shared)

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,46 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroSectionComponent } from './shared/hero-section/hero-section.component';
-import { ListGroupComponent } from './shared/list-group/list-group.component';
-import { ListGroupItemComponent } from './shared/list-group-item/list-group-item.component';
-import { HeadingOneComponent } from './shared/heading-one/heading-one.component';
-import { HeadingTwoComponent } from './shared/heading-two/heading-two.component';
-import { HeadingThreeComponent } from './shared/heading-three/heading-three.component';
-import { HeadingFourComponent } from './shared/heading-four/heading-four.component';
-import { HeadingFiveComponent } from './shared/heading-five/heading-five.component';
-import { HeadingSixComponent } from './shared/heading-six/heading-six.component';
-import { DescriptionComponent } from './shared/description/description.component';
-import { HeroSectionSummaryComponent } from './shared/hero-section-summary/hero-section-summary.component';
-import { HeroSectionHeadingComponent } from './shared/hero-section-heading/hero-section-heading.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeModule } from './home/home.module';
+import { NotFoundModule } from './not-found/not-found.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroSectionComponent,
-    ListGroupComponent,
-    ListGroupItemComponent,
-    HeadingOneComponent,
-    HeadingTwoComponent,
-    HeadingThreeComponent,
-    HeadingFourComponent,
-    HeadingFiveComponent,
-    HeadingSixComponent,
-    DescriptionComponent,
-    HeroSectionSummaryComponent,
-    HeroSectionHeadingComponent,
-    HomeComponent,
-    NotFoundComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: '**', component: NotFoundComponent }
-    ])
+    ]),
+    HomeModule,
+    NotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
