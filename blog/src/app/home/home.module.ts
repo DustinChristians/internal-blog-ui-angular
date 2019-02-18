34b89e9f654b86066b75cb65b646b22eaa50a8ea
Dustin <dustinchristians@gmail.com>
2019-02-18 07:46:15 -0500

    Added child routes to a new home-routing module.

--- blog/src/app/home/home.module.ts
+++ blog/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
