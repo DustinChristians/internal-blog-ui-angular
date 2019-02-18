2d24bf12ec469f2d56edceed235f59d8928def86
Dustin <dustinchristians@gmail.com>
2019-02-18 07:38:13 -0500

    Moved the root application routing into it's own app-routing.module

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeModule } from './home/home.module';
 import { NotFoundModule } from './not-found/not-found.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: '**', component: NotFoundComponent }
-    ]),
     HomeModule,
-    NotFoundModule
+    NotFoundModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
