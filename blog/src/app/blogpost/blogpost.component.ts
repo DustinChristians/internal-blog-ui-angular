e7302e24272d74e1b9008e9cd068b11ad9d8c0a8
Dustin <dustinchristians@gmail.com>
2019-06-20 06:09:36 -0400

    Added new markup and styling for blog posts.

--- blog/src/app/blogpost/blogpost.component.ts
+++ blog/src/app/blogpost/blogpost.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 import { ContentService, Entry } from '../core/content/content.service';
 import { ActivatedRoute } from '@angular/router';
 import { CmsMap } from '../core/cms/cms.map';
@@ -13,16 +13,35 @@ import { ErrorService } from '../core/error/error.service';
   templateUrl: './blogpost.component.html',
   styleUrls: ['./blogpost.component.scss']
 })
-export class BlogpostComponent implements OnInit {
+export class BlogpostComponent implements OnInit, AfterViewInit {
   blogPost: BlogPost = new BlogPost();
   errorMessage: string;
   heroSection: HeroSection = new HeroSection();
+  sidebar;
 
   constructor(
     private contentService: ContentService,
     private errorService: ErrorService,
     private route: ActivatedRoute
   ) {}
+
+  // for window scroll events
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.displaySidebar();
+  }
+
+  displaySidebar() {
+    const hero = document.getElementById('js-hero-section');
+    const sidebar = document.getElementById('js-social-sidebar');
+    const heroRect = hero.getBoundingClientRect();
+
+    if (window.scrollY - heroRect.height > 0) {
+      sidebar.classList.remove('hidden');
+    } else {
+      sidebar.classList.add('hidden');
+    }
+  }
 
   ngOnInit() {
     this.getEntry();
